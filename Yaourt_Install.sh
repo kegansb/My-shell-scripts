be3aa7271774dfc2e4c8e959b366acbef5a0ab1f
Kegan Brauning <kegansb@users.noreply.github.com>
2016-09-19 19:35:37 -0400

    Add files via upload

---
+++ Yaourt_Install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#This script will enable Yaourt to run on your system
+
+sudo pacman -S --needed base-devel git wget yajl
+
+git clone https://aur.archlinux.org/package-query.git
+
+cd package-query/
+
+makepkg -si
+
+cd ..
+
+git clone https://aur.archlinux.org/yaourt.git
+
+cd yaourt
+
+makepkg -si
+
+cd ..
+
+sudo rm -dR yaourt/ package-query/
